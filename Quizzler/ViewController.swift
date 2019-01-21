2eb97a20b6faf7cca62b88532aeaf5f5f2360831
ReynaDuong <minhduong.md25@gmail.com>
2019-01-21 15:57:48 -0600

    add data and model

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -2,18 +2,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let allQuestions = QuestionBank()
+    var pickedAnswer: Bool = false
+    
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // display the first question
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
+        if sender.tag == 1{
+            pickedAnswer = true
+        }
+        else{
+            pickedAnswer = false
+        }
         
+        // check the answer of user
+        checkAnswer()
     }
     
     
